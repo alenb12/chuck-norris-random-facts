eb27b94704b34a1570e2d87363ee05ca1ddf111e
alenb12 <alen.bedek@gmail.com>
2020-07-14 19:11:13 +0200

    Updates

---
+++ precache-manifest.664daa960317bb0aa8162996d220526b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82f243762b2cc20f03c34dbad56067f6",
+    "url": "/chuck-norris-random-facts/index.html"
+  },
+  {
+    "revision": "6db3b8d7640ddbc501c1",
+    "url": "/chuck-norris-random-facts/static/css/main.cec70d77.chunk.css"
+  },
+  {
+    "revision": "803d1ab367e6c439cbcf",
+    "url": "/chuck-norris-random-facts/static/js/2.4fcef3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chuck-norris-random-facts/static/js/2.4fcef3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db3b8d7640ddbc501c1",
+    "url": "/chuck-norris-random-facts/static/js/main.123c83f5.chunk.js"
+  },
+  {
+    "revision": "0c19ebf4e930bf122f74",
+    "url": "/chuck-norris-random-facts/static/js/runtime-main.16167b7d.js"
+  }
+]);
